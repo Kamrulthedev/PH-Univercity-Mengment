2e8203cbe1713d1de5fdb955cd5c33d2f86afeb9
Kamrul Hassan <kamrulthedev@gmail.com>
2024-05-17 20:39:31 +0600

    prettier-commit

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,13 @@
-import cors from 'cors';
-import express, { Application, Request, Response } from 'express';
-const app:Application = express()
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
 
+app.use(express.json());
+app.use(cors());
 
-app.use(express.json())
-app.use(cors())
-
-app.get('/', (req : Request, res: Response) => {
-  const a = 19
-  res.send(a)
+app.get("/", (req: Request, res: Response) => {
+  const a = 19;
+  res.send(a);
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,14 @@ import config from "./app/config";
 import app from "./app";
 
 async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.prot, () => {
+      console.log(`Example app listening on port ${config.prot}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-    try{
-        await mongoose.connect(config.database_url as string);
-        app.listen(config.prot, () => {
-          console.log(`Example app listening on port ${config.prot}`)
-      });
-    }catch(error){
-        console.log(error)
-    }
-}; 
-
-main()
-
+main();
