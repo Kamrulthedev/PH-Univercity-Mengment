cbf56faca24858b0fa0701839c05c754cb9937c9
Kamrul Hassan <kamrulthedev@gmail.com>
2024-05-28 08:50:50 +0600

    academic-semester-commit

--- src/app/modules/Users/user.routes.ts
+++ src/app/modules/Users/user.routes.ts
@@ -5,10 +5,6 @@ import validationRequest from "../../milddlerwer/validate.Request";
 
 const router = express.Router();
 
-router.post(
-  "/create-student",
-  validationRequest(studentvalidations.createStudentvalidationSchema),
-  UserControllar.createStudentDb
-);
+router.post("/create-student",validationRequest(studentvalidations.createStudentvalidationSchema),UserControllar.createStudentDb);
 
 export const UserRouotes = router;
