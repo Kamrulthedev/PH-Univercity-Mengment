8f9f75d686681f41117f87e3567e99c19bf47674
Kamrul Hassan <kamrulthedev@gmail.com>
2024-05-28 12:08:02 +0600

    Error-handeling-commit

--- src/app/modules/academicSemester/academicSemester.interface.ts
+++ src/app/modules/academicSemester/academicSemester.interface.ts
@@ -21,7 +21,7 @@ export type TMonths =
 export type TAcademicSemester = {
   name:TAcademicSemesterName;
   code: TAcademicSemesterCode;
-  year: Date;
+  year: string;
   startMonth: TMonths;
   endMonth: TMonths;
 };
