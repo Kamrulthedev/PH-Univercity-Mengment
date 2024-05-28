53ed8b8c8834887570134a6ccc1f7612888a051d
Kamrul Hassan <kamrulthedev@gmail.com>
2024-05-28 07:24:31 +0600

    update-commit

--- src/app/modules/student/student.controlar.ts
+++ src/app/modules/student/student.controlar.ts
@@ -1,39 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import { StudentService } from "./student.service";
-
-
-
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
 
 //gell all students
-const getStudents = (async (req:Request, res:Response, next:NextFunction) => {
+const getStudents = catchAsync(async (req, res, next) => {
   const result = await StudentService.getAllStudentsFromDB();
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    message: "Student are retrieved Successfully",
+    message: "Student are retrieved successfully",
     data: result,
   });
 });
 
 //get a single student
-const getASingleStudent =  (async (req:Request, res:Response, next:NextFunction)=> {
+const getASingleStudent = catchAsync(async (req, res, next) => {
   const { studentId } = req.params;
   const result = await StudentService.GetASingleStudent(studentId);
-  res.status(200).json({
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
     success: true,
-    message: "A Single Student Get is Successfully",
+    message: " Single Student retrieved successfully",
     data: result,
   });
 });
 
 //delete a student
-const deleteStudent =  (async (req:Request, res:Response, next:NextFunction) => {
+const deleteStudent = catchAsync(async (req, res, next) => {
   const studentId = req.params.studentId;
   const result = await StudentService.deleteStudentformDB(studentId);
-  res.status(200).json({
-    success: true,
-    message: "Delete Student is Successfully",
-    data: result,
-  });
+  sendResponse(res, {
+    statusCode:httpStatus.OK,
+    success:true,
+    message:"Student Deleted successfully",
+    data:result
+  })
 });
 
 //export student Controllar
