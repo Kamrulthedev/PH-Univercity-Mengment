d0c26d8c9a38277a4850dc5649b63708f4fab191
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-03 18:47:37 +0600

    error-hendleing-commit

--- src/app/milddlerwer/globalErrorHandelar.ts
+++ src/app/milddlerwer/globalErrorHandelar.ts
@@ -1,14 +1,10 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
-import { ZodError } from "zod";
+import { ZodError, ZodIssue } from "zod";
+import { TErrorSource } from "../interface/error";
 
 const GlobalErrorHandel: ErrorRequestHandler = (err, req, res, next) => {
   let statusCode = err.statusCode || 500;
-  let message = err.message  || "Somoting went wromng !";
-
-  type TErrorSource = {
-    path: string | number;
-    message: string;
-  };
+  let message = err.message || "Somoting went wromng !";
 
   let errorSources: TErrorSource = [
     {
@@ -17,15 +13,34 @@ const GlobalErrorHandel: ErrorRequestHandler = (err, req, res, next) => {
     },
   ];
 
-  if(err instanceof ZodError) {
-    statusCode = 400,
-    message = "AMi ZOD ERROR"
-  }
+  // Define your error handler for Zod errors
+  const handlerZodError = (err: ZodError) => {
+    const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+      return {
+        path: issue.path[issue.path.length - 1],
+        message: issue.message,
+      };
+    });
+    const statusCode = 400;
+
+    return {
+      statusCode,
+      message: "Zod Validation Error",
+      errorSources,
+    };
+  };
+
+  if (err instanceof ZodError) {
+    const simplifiedError = handlerZodError(err);
+    statusCode = simplifiedError.statusCode;
+     message = simplifiedError.message;
+     errorSources = simplifiedError.errorSources;
+  };
+
   return res.status(statusCode).json({
     sucess: false,
     message,
     errorSources,
-    amiError: err,
   });
 };
 
