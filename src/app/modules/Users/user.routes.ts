049e026b82ad9b7afce45a707c07584d588b32ea
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-06 11:02:01 +0600

    validation-with-faculty

--- src/app/modules/Users/user.routes.ts
+++ src/app/modules/Users/user.routes.ts
@@ -2,11 +2,12 @@ import express from "express";
 import { UserControllar } from "./user.cotrollar";
 import { studentvalidations } from "../student/student.validation";
 import validationRequest from "../../milddlerwer/validate.Request";
+import { FacultyValidationSchema } from "../Faculty/faculty.validation";
 
 const router = express.Router();
 
 router.post("/create-student",validationRequest(studentvalidations.createStudentValidationSchema),UserControllar.createStudentDb);
 
-router.post('/create-faculty', UserControllar.createfacultyDb)
+router.post('/create-faculty',validationRequest(FacultyValidationSchema.createFacultyValidationSchema), UserControllar.createfacultyDb)
 
 export const UserRouotes = router;
