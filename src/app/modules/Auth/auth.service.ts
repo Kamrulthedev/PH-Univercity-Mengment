9fad66aeacedd15d533db2f57df119d28c03dc74
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-13 10:27:53 +0600

    This user is blocked-chack

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,24 +2,32 @@ import httpStatus from "http-status";
 import AppError from "../../Error/AppError";
 import { User } from "../users/user.model";
 import { TLoginUser } from "./auth.interface";
+import bcrypt from "bcrypt";
 
 const loginUser = async (payload: TLoginUser) => {
   const isExistsUser = await User.findOne({ id: payload?.id });
 
   if (!isExistsUser) {
     throw new AppError(httpStatus.NOT_FOUND, "This user is Not Found !!");
-  };
+  }
 
   const deleteUser = isExistsUser?.isDeleted;
   if (deleteUser) {
     throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !!");
-  };
+  }
 
   //chack user status
   const UserSataus = isExistsUser?.status;
-  if (UserSataus === 'blocked') {
+  if (UserSataus === "blocked") {
     throw new AppError(httpStatus.FORBIDDEN, "This user is blocked !!");
-  };
+  }
+
+  //checking if the password currant
+  const ispasswordMaths = await bcrypt.compare(
+    payload?.password,
+    isExistsUser?.password
+  );
+  console.log(ispasswordMaths);
 
   return {};
 };
