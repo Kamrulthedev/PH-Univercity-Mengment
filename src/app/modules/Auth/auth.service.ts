0467177c6cd5277c23f3d03fffa424d8101ec7c7
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-14 06:29:31 +0600

    create-json-web-token

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,7 +2,7 @@ import httpStatus from "http-status";
 import AppError from "../../Error/AppError";
 import { User } from "../users/user.model";
 import { TLoginUser } from "./auth.interface";
-import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
 
 const loginUser = async (payload: TLoginUser) => {
   const isExistsUser = await User.isUserExsitsByCustomId(payload.id);
@@ -11,16 +11,17 @@ const loginUser = async (payload: TLoginUser) => {
     throw new AppError(httpStatus.NOT_FOUND, "This user is Not Found !!");
   }
 
-    const deleteUser = isExistsUser?.isDeleted;
-    if (deleteUser) {
-      throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !!");
-    }
+  //chack user status
+  const deleteUser = isExistsUser?.isDeleted;
+  if (deleteUser) {
+    throw new AppError(httpStatus.FORBIDDEN, "This user is deleted !!");
+  }
 
-    //chack user status
-    const UserSataus = isExistsUser?.status;
-    if (UserSataus === "blocked") {
-      throw new AppError(httpStatus.FORBIDDEN, "This user is blocked !!");
-    }
+  //chack user status
+  const UserSataus = isExistsUser?.status;
+  if (UserSataus === "blocked") {
+    throw new AppError(httpStatus.FORBIDDEN, "This user is blocked !!");
+  }
 
   // Verify password
   const isPasswordValid = await User.isPasswordMaths(
@@ -29,7 +30,17 @@ const loginUser = async (payload: TLoginUser) => {
   );
   if (!isPasswordValid) {
     throw new AppError(httpStatus.UNAUTHORIZED, "Invalid password");
-  }
+  };
+
+//create token and sent to the user
+
+const jwrPayload = {
+  userId : isExistsUser,
+  role: isExistsUser.role
+}
+const accessToken = jwt.sign()
+
+
 
   return {};
 };
