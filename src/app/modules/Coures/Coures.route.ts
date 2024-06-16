0d0c2bad2bc9b5c53af8fa07f42bfa7a2aeef505
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-16 11:47:48 +0600

    update-course-route

--- src/app/modules/Coures/Coures.route.ts
+++ src/app/modules/Coures/Coures.route.ts
@@ -2,37 +2,47 @@ import express from "express";
 import { CouresControllar } from "./Coures.controllar";
 import validationRequest from "../../milddlerwer/validate.Request";
 import { CouresValidation } from "./Coures.validation";
+import AuthValidated from "../../milddlerwer/auth.validated";
 
 const router = express.Router();
 
 router.post(
   "/create-course",
+  AuthValidated("admin"),
   validationRequest(CouresValidation.CouresValidationSchema),
-  CouresControllar.createCouresDb,
+  CouresControllar.createCouresDb
 );
 
-router.get("/", CouresControllar.getAllCouresDb);
+router.get(
+  "/",
+  CouresControllar.getAllCouresDb
+);
 
-router.get("/:id", CouresControllar.getSingleCouresDb);
+router.get(
+  "/:id",
+  AuthValidated("student", "faculty", "admin"),
+  CouresControllar.getSingleCouresDb
+);
 
-router.delete("/:id", CouresControllar.deleteCouresDb);
+router.delete("/:id", AuthValidated("admin"), CouresControllar.deleteCouresDb);
 
 router.put(
   "/:courseId/assign-faculties",
   validationRequest(CouresValidation.assignFacultiseWithCourseValidaitonSchema),
-  CouresControllar.assignFacultiseWithCourseDb,
+  CouresControllar.assignFacultiseWithCourseDb
 );
 
 router.delete(
   "/:courseId/remove-faculties",
   validationRequest(CouresValidation.assignFacultiseWithCourseValidaitonSchema),
-  CouresControllar.removeFacultyWithCourseDb,
+  CouresControllar.removeFacultyWithCourseDb
 );
 
 router.patch(
   "/:id",
+  AuthValidated("admin"),
   validationRequest(CouresValidation.UpdateCouresValidationSchema),
-  CouresControllar.updateCorseDb,
+  CouresControllar.updateCorseDb
 );
 
 export const CouresRauter = router;
