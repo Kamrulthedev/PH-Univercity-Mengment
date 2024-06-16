7da199064780c5e88e220c4062fbdc6a4241c2b0
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-16 12:09:54 +0600

    Prepare for Vercel deployment

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,32 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
-import GlobalErrorHandel from "./app/milddlerwer/globalErrorHandelar";
-import notFound from "./app/milddlerwer/notFound";
-import router from "./app/routes";
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
+import cors from 'cors';
+import express, { Application, Request, Response, NextFunction } from 'express';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import router from './app/routes';
+import notFound from './app/milddlerwer/notFound';
+import GlobalErrorHandel from './app/milddlerwer/globalErrorHandelar';
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin:['http://localhost:5173']}));
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
-//application routes
-app.use("/api/v1", router);
+// Application routes
+app.use('/api/v1', router);
 
-app.get("/", (req: Request, res: Response) => {
-  const result = "Hello !!!";
-  res.send(result);
+app.get('/', (req: Request, res: Response) => {
+    const result = 'Hello !!!';
+    res.send(result);
 });
 
+// Global error handler
 app.use(GlobalErrorHandel);
+
+// Not found handler
 app.use(notFound);
 
 export default app;
