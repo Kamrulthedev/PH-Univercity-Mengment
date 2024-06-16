7dee5881a4b1cbb6a323416da6c12a324f1e264c
Kamrul Hassan <kamrulthedev@gmail.com>
2024-06-16 12:22:49 +0600

    update-vercel-daploy

--- dist/app.js
+++ dist/app.js
@@ -5,23 +5,25 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
-const globalErrorHandelar_1 = __importDefault(require("./app/milddlerwer/globalErrorHandelar"));
-const notFound_1 = __importDefault(require("./app/milddlerwer/notFound"));
-const routes_1 = __importDefault(require("./app/routes"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const routes_1 = __importDefault(require("./app/routes"));
+const notFound_1 = __importDefault(require("./app/milddlerwer/notFound"));
+const globalErrorHandelar_1 = __importDefault(require("./app/milddlerwer/globalErrorHandelar"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({ origin: ['http://localhost:5173'] }));
 // Middleware to parse JSON bodies
 app.use(body_parser_1.default.json());
-//application routes
-app.use("/api/v1", routes_1.default);
-app.get("/", (req, res) => {
-    const result = "Hello !!!";
+// Application routes
+app.use('/api/v1', routes_1.default);
+app.get('/', (req, res) => {
+    const result = 'Hello !!!';
     res.send(result);
 });
+// Global error handler
 app.use(globalErrorHandelar_1.default);
+// Not found handler
 app.use(notFound_1.default);
 exports.default = app;
--- src/app/Error/handleZodError.ts
+++ src/app/Error/handleZodError.ts
@@ -5,7 +5,7 @@ import { TErrorSources } from "../interface/error";
 const handlerZodError = (err: ZodError) => {
   const errorSources: TErrorSources = err.issues.map((issue: ZodIssue) => {
     return {
-      path: issue.path[issue.path.length - 1],
+      path: issue.path[issue.path.length - 1] as string, // Ensure path is a string
       message: issue.message,
     };
   });
